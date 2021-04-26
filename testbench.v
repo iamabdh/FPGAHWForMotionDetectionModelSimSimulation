a38c1fdb205d0d3e30588f2ea71157df5950abc2
Abdullah <abdullahalbadri09@gmail.com>
2021-04-26 22:31:04 +0400

    edit testbench

--- testbench.v
+++ testbench.v
@@ -3,17 +3,18 @@
 module testbench();
 
 wire clk;
+wire turn;
 wire stop_alarm;
 wire pir_sensor_1;
 wire pir_sensor_2;
 wire pir_sensor_3;
-wire LED;
+wire [2:0] LED;
 wire buzzer;
 wire [20:0] display_data;
 
-pir c1 (clk, stop_alarm, pir_sensor_1, pir_sensor_2, pir_sensor_3, LED, buzzer, display_data);
-tester t1 (clk, stop_alarm, pir_sensor_1, pir_sensor_2, pir_sensor_3);
+pir c1 (clk, turn, stop_alarm, pir_sensor_1, pir_sensor_2, pir_sensor_3, LED, buzzer, display_data);
+tester t1 (clk, turn, stop_alarm, pir_sensor_1, pir_sensor_2, pir_sensor_3);
 
-    initial $monitor ($time, "clk = %b, stop_alarm = %b, pir_sensor_1 = %b, pir_sensor_2 = %b, pir_sensor_3 = %b, LED = %b, buzzer = %b, display_data = %b", clk, stop_alarm, pir_sensor_1, pir_sensor_2, pir_sensor_3, LED, buzzer, display_data);
+    initial $monitor ($time, "clk = %b, turn = %b, stop_alarm = %b, pir_sensor_1 = %b, pir_sensor_2 = %b, pir_sensor_3 = %b, LED = %b, buzzer = %b, display_data = %b", clk, turn, stop_alarm, pir_sensor_1, pir_sensor_2, pir_sensor_3, LED, buzzer, display_data);
 
 endmodule
