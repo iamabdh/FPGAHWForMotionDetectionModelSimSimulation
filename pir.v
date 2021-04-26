a9f70fbcf6abe17603bfc62610868d83762c69f7
iamabdh <abdullahalbadri09@gmail.com>
2021-04-27 03:33:33 +0400

    fix errors

--- pir.v
+++ pir.v
@@ -68,7 +68,8 @@ always @(posedge clk) begin
             counter_buzzing <= 0;
             buzzer <=0;
             nth_sensor_triggered <=0;
-            RAM[0:7] <= 0;
+            RAM[0] <= 0;
+            RAM[1] <= 0;
             if (turn == 1) begin
                 fsm_state <= IDLE;
             end
