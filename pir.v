174e09fb2c7731a35958a5e6ea6446668b4d8fe3
iamabdh <abdullahalbadri09@gmail.com>
2021-04-28 02:47:24 +0400

    edit storage

--- pir.v
+++ pir.v
@@ -27,7 +27,7 @@ input   [6:0]   pir_sensor_3;
 
 output reg  [2:0]  LED;  
 output reg         buzzer;
-output reg [20:0]  display_data;
+output reg [30:0]  display_data;
 
 // ----------------
 // Lacal parameters 
@@ -36,7 +36,7 @@ output reg [20:0]  display_data;
 localparam INIT             = 4'b0001;
 localparam IDLE             = 4'b0010;
 localparam BUZZRING         = 4'b0100;
-localparam STOPING_ALARM    = 4'b1000;
+localparam STOPING_ALARM_DEVICE    = 4'b1000;
 
 // Period of Operation (in clock cycles)
 localparam BUZZING_DELAY 	= 100;
@@ -48,11 +48,19 @@ localparam BUZZING_DELAY 	= 100;
 reg [3:0] fsm_state = INIT;
 reg [6:0] counter_buzzing;
 reg [2:0] nth_sensor_triggered;
-
+reg [7:0] total; // used for total number of sensor are triggered 
 // ----------------
 // RAM  8x8
 // ----------------
 reg [7:0] RAM [0:7];
+
+integer i;
+initial begin
+    for(i=0; i<8; i = i+1) begin
+        RAM[i] = 0; // initial all data to zero
+    end
+end
+
 
 // ----------------
 // Controller
@@ -68,8 +76,7 @@ always @(posedge clk) begin
             counter_buzzing <= 0;
             buzzer <=0;
             nth_sensor_triggered <=0;
-            RAM[0] <= 0;
-            RAM[1] <= 0;
+            total <= 0;
             if (turn == 1) begin
                 fsm_state <= IDLE;
             end
@@ -80,14 +87,18 @@ always @(posedge clk) begin
                 // display some data while idling=> 
                 // threshold, threshold from  which sensor, 
                 // last measurment, last from  which sensor.
-                display_data[11:4] <= RAM[0];
-                display_data[15:12] <= RAM[1];
+                display_data[7:0]   <=  RAM[0];
+                display_data[11:8]  <=  RAM[1];
+                display_data[19:12] <=  RAM[2];
+                display_data[23:20] <=  RAM[3];
+                display_data[30:23] <=  RAM[4];
                  if (pir_sensor_1 >= 50 | pir_sensor_2 >= 50 | pir_sensor_3 >= 50) begin
+                     display_data <=0;
                     fsm_state  <=  BUZZRING;
                 end 
             end 
             else if (turn == 0) begin
-                    fsm_state <= INIT; 
+                    fsm_state <= STOPING_ALARM_DEVICE; 
             end
         end
 
@@ -96,18 +107,24 @@ always @(posedge clk) begin
             if (pir_sensor_1 >= 50) begin
                 LED[0] <=1;
                 nth_sensor_triggered[0] <=1;
+                RAM[2] = pir_sensor_1;
+                RAM[3] = 1;
             end
             if (pir_sensor_2 >= 50) begin
                 LED[1] <=1;
                 nth_sensor_triggered[1] <=1;
+                RAM[2] = pir_sensor_2;
+                RAM[3] = 2;
             end
             if (pir_sensor_3 >= 50) begin
                 LED[2] <=1;
                 nth_sensor_triggered[2] <=1;
+                RAM[2] = pir_sensor_3;
+                RAM[3] = 3;
             end
             
-            //  display how many sensor triggered from 0 to 3 bit in display_data
-            display_data[3:0] <= nth_sensor_triggered[0] + nth_sensor_triggered[1] + nth_sensor_triggered[2];
+            //  store total nth sensor triggered
+            total <= nth_sensor_triggered[0] + nth_sensor_triggered[1] + nth_sensor_triggered[2];
 
             // store threshold address 0 in RAM, address 1 store from which sensor 
             if (pir_sensor_1 >= pir_sensor_2 & pir_sensor_1 >= pir_sensor_3 & pir_sensor_1 >= RAM[0]) begin
@@ -129,19 +146,14 @@ always @(posedge clk) begin
                 counter_buzzing <= 0;
                 fsm_state <= STOPING_ALARM;
             end
-            if (stop_alarm == 1) begin
+            if (stop_alarm == 1 | turn == 0 ) begin
                 counter_buzzing <= 0;
-                fsm_state <= STOPING_ALARM;
+                fsm_state <= STOPING_ALARM_DEVICE;
             end 
-            else if (turn == 0) begin
-                fsm_state <= INIT; 
-            end
         end
-        STOPING_ALARM: begin
-            LED <= 0;
-            buzzer <= 0;
-            nth_sensor_triggered <=0;
-            fsm_state <= IDLE;
+        STOPING_ALARM_DEVICE: begin
+            RAM[4] <= RAM[4] + total; // store number of sensot are triggried
+            fsm_state <= INIT;
         end
     endcase
 end   
