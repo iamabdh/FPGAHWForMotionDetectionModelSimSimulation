22f17f226e19915cdb670e9c2eb7a0c37b54cd2a
Abdullah <abdullahalbadri09@gmail.com>
2021-05-07 09:42:47 +0400

    change time line

--- tester.v
+++ tester.v
@@ -2,30 +2,23 @@
 
 module tester
 (
-  output reg        clk,
+  input             clk,
   output reg        turn,
   output reg        stop_alarm,
   output reg [6:0]  pir_sensor_1,
   output reg [6:0]  pir_sensor_2,
   output reg [6:0]  pir_sensor_3
 );
-initial clk = 1'b0;
-always 
-begin
-    #10 
-    clk = ~clk;
-end
 
 initial begin 
     stop_alarm   = 0;
     pir_sensor_1 = 0;
     pir_sensor_2 = 0;
-    pir_sensor_3 = 0;
+      pir_sensor_3 =  56;
     turn  = 'b1;
     #50
     pir_sensor_1 =  29;
-    pir_sensor_3 =  56;
-    #100
+    #1000
     pir_sensor_1 = 0;
     pir_sensor_3 = 0;
     stop_alarm = 1;
