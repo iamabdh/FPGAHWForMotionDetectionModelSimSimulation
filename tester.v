5e1003bb02155f4fabdec19d2c0d1dfcf2298b50
Abdullah <abdullahalbadri09@gmail.com>
2021-05-10 11:10:41 +0400

    update tester

--- tester.v
+++ tester.v
@@ -14,22 +14,46 @@ initial begin
     stop_alarm   = 0;
     pir_sensor_1 = 0;
     pir_sensor_2 = 0;
-      pir_sensor_3 =  56;
+    pir_sensor_3 = 0;
     turn  = 'b1;
     #50
-    pir_sensor_1 =  29;
-    #1000
+    pir_sensor_1 =  69;
+    pir_sensor_2 =  80;
+    pir_sensor_3 =  62;
+    #80
+    pir_sensor_1 =  99;
+    pir_sensor_2 =  70;
+    pir_sensor_3 =  84;
+    #80
+    pir_sensor_1 =  99;
+    pir_sensor_2 =  70;
+    pir_sensor_3 =  60;
+    #80
+    pir_sensor_1 =  80;
+    pir_sensor_2 =  10;
+    pir_sensor_3 =  70;
+    #120
+    pir_sensor_1 = 0;
+    pir_sensor_2 = 0;
+    pir_sensor_3 = 0;
+    #200
+    pir_sensor_1 = 30;
+    pir_sensor_3 = 30;
+    pir_sensor_2 = 51;
+    #200 
+    stop_alarm = 0;
+    #20
+    stop_alarm = 0;
+    #100
     pir_sensor_1 = 0;
     pir_sensor_3 = 0;
-    stop_alarm = 1;
-    #100
-    stop_alarm ='b0;
-    turn ='b0;
-    #100
-    turn ='b1;
-    #1000
+    pir_sensor_2 = 0;
+    #10200
     pir_sensor_2 = 90;
-    #100
+    #300
+    stop_alarm = 1;
+    #20
+    stop_alarm = 0;
     pir_sensor_2 = 0;
     #1000
     #3000
